1be669892be5f84755b7b4ade2ef1537663cbbcd
123abdel321 <139260336+123abdel321@users.noreply.github.com>
2023-08-13 22:58:14 -0500

    border header tables

--- resources/views/pages/tablas/comprobantes/comprobantes-table.blade.php
+++ resources/views/pages/tablas/comprobantes/comprobantes-table.blade.php
@@ -1,12 +1,12 @@
 <table id="comprobantesTable" class="table table-bordered display responsive" width="100%">
     <thead>
         <tr>
-            <th>Código</th>
+            <th style="border-radius: 15px 0px 0px 0px !important;">Código</th>
             <th>Nombre</th>
             <th>Tipo comprobante</th>
             <th>Tipo consecutivo</th>
             <th>Consecutivo siguiente</th>
-            <th>Acciones</th>
+            <th style="border-radius: 0px 15px 0px 0px !important;">Acciones</th>
         </tr>
     </thead>
 </table>
