f91e6fa6b27b91d89de6dc637e6815dffce57e18
Abdel cartagena castro <abdel_123@hotmail.es>
2024-01-08 00:48:43 -0500

    Agregar documento_referencia en bulkDocumentos

--- app/Http/Controllers/Capturas/DocumentoGeneralController.php
+++ app/Http/Controllers/Capturas/DocumentoGeneralController.php
@@ -145,6 +145,7 @@ class DocumentoGeneralController extends Controller
 						$docGeneral['id_nit'] = $doc->id_tercero_erp;
 						$docGeneral['id_cuenta'] = $cuentaContable->id;
 						$docGeneral['id_centro_costos'] = $doc->id_centro_costos;
+						$docGeneral['documento_referencia'] = $doc->documento_referencia;
 						$docGeneral['consecutivo'] = $doc->consecutivo_factura;
 						$docGeneral['created_by'] = request()->user()->id;
 						$docGeneral['updated_by'] = request()->user()->id;
