fde49692deea4b15d42535560ce0d0e2a9c973f1
Abdel cartagena castro <abdel_123@hotmail.es>
2024-01-10 20:28:27 -0500

    Actualizar semilla de formas pagos

--- database/seeders/Provisionada/FormasPagosSeeder.php
+++ database/seeders/Provisionada/FormasPagosSeeder.php
@@ -15,25 +15,37 @@ class FormasPagosSeeder extends Seeder
                 'id' => 1,
                 'id_cuenta' => 4,
                 'id_tipo_formas_pago' => 10,
-                'nombre' => 'EFECTIVO'
+                'nombre' => 'CAJA EFECTIVO'
             ],
             [
                 'id' => 2,
-                'id_cuenta' => 6,
-                'id_tipo_formas_pago' => 14,
+                'id_cuenta' => 2792,
+                'id_tipo_formas_pago' => 31,
                 'nombre' => 'TRANSFERENCIA'
             ],
             [
                 'id' => 3,
-                'id_cuenta' => 9,
+                'id_cuenta' => 150,
                 'id_tipo_formas_pago' => 1,
-                'nombre' => 'CRÉDITO'
+                'nombre' => 'VENTAS CREDITO'
             ],
             [
                 'id' => 4,
-                'id_cuenta' => 70,
+                'id_cuenta' => 1081,
                 'id_tipo_formas_pago' => 1,
-                'nombre' => 'ANTICIPOS'
+                'nombre' => 'ANTICIPOS CLIENTES'
+            ],
+            [
+                'id' => 5,
+                'id_cuenta' => 802,
+                'id_tipo_formas_pago' => 1,
+                'nombre' => 'COMPRAS CREDITO'
+            ],
+            [
+                'id' => 6,
+                'id_cuenta' => 272,
+                'id_tipo_formas_pago' => 1,
+                'nombre' => 'ANTICIPOS PROVEEDORES'
             ],
         ]);
     }
