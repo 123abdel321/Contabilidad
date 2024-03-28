0bedc377a682a7e444393861805dfee3c0197d45
Abdel cartagena castro <abdel_123@hotmail.es>
2024-03-27 19:44:43 -0500

    Agregar front de gastos sin terminar guardado

---
+++ resources/views/pages/capturas/gasto/gasto-table.blade.php
@@ -0,0 +1,15 @@
+<table id="gastoTable" class="table table-bordered display table-captura-gasto responsive" width="100%">
+    <thead>
+        <tr>
+            <th style="border-radius: 15px 0px 0px 0px !important;"><i class="fas fa-sliders-h"></i></th>
+            <th>Concepto</th>
+            <th>Valor</th>
+            <th>% Dscto</th>
+            <th>Dscto</th>
+            <th>Iva</th>
+            <th>Retención</th>
+            <th>Total gasto</th>
+            <th style="border-radius: 0px 15px 0px 0px !important;">Observación</th>
+        </tr>
+    </thead>
+</table>
