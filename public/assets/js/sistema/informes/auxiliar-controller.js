9657a434af1849439897a0a4329e2cfbbc5ecba4
Abdel cartagena castro <abdel_123@hotmail.es>
2024-05-20 07:56:03 -0500

    Actualizar auxiliar

--- public/assets/js/sistema/informes/auxiliar-controller.js
+++ public/assets/js/sistema/informes/auxiliar-controller.js
@@ -147,12 +147,12 @@ function auxiliarInit() {
                 var saldo_final = parseFloat(row.saldo_final);
                 if(row.naturaleza_cuenta == 0 && saldo_final < 0 && row.detalle_group == 'nits') {
                     var cuenta = row.cuenta.charAt(0)+row.cuenta.charAt(1);
-                    if (cuenta == '11') {
+                    if (!cuenta == '11') {
                         return '<div class=""><i class="fas fa-exclamation-triangle error-triangle"></i>&nbsp;'+(saldo_final*-1).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')+'</div>';
                     }
                 } else if(row.naturaleza_cuenta == 1 && saldo_final > 0 && row.detalle_group == 'nits') {
                     var cuenta = row.cuenta.charAt(0)+row.cuenta.charAt(1);
-                    if (cuenta == '11') {
+                    if (!cuenta == '11') {
                         return '<div class=""><i class="fas fa-exclamation-triangle error-triangle"></i>&nbsp;'+(saldo_final).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')+'</div>';
                     }
                 } else if(row.naturaleza_cuenta == 1 && saldo_final < 0) {
