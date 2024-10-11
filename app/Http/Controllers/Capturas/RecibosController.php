fbe18f94a9b1fbfaac7638b724bc91adcf93fd89
Abdel cartagena castro <abdel_123@hotmail.es>
2024-10-11 08:16:26 -0500

    Validar que exista la fecha manual en recibos

--- app/Http/Controllers/Capturas/RecibosController.php
+++ app/Http/Controllers/Capturas/RecibosController.php
@@ -247,13 +247,13 @@ class RecibosController extends Controller
         $empresa = Empresa::where('id', request()->user()->id_empresa)->first();
 		$fechaCierre= DateTimeImmutable::createFromFormat('Y-m-d', $empresa->fecha_ultimo_cierre);
 
-        // if ($this->fechaManual < $fechaCierre) {
-		// 	return response()->json([
-        //         "success"=>false,
-        //         'data' => [],
-        //         "message"=>['fecha_manual' => ['mensaje' => 'Se esta grabando en un año cerrado']]
-        //     ], 422);
-		// }
+        if (!$request->get('fecha_manual')) {
+			return response()->json([
+                "success"=>false,
+                'data' => [],
+                "message"=>['fecha_manual' => ['mensaje' => 'La fecha es incorrecta']]
+            ], 422);
+		}
         
         try {
             DB::connection('sam')->beginTransaction();
