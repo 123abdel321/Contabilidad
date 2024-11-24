76610f24c918eb43c2256b627f6d17107677f7b0
Abdel cartagena castro <abdel_123@hotmail.es>
2024-11-23 19:28:13 -0500

    Actualizar columnas de balance

--- resources/views/pages/contabilidad/balance/balance-filter.blade.php
+++ resources/views/pages/contabilidad/balance/balance-filter.blade.php
@@ -13,7 +13,7 @@
                 <form id="balanceprimaryrmeForm" style="margin-top: 10px;">
                     <input name="id_balance_cargado" id="id_balance_cargado" class="form-control form-control-sm" type="text" style="display: none;">
                     <div class="row">
-                        <div class="form-group form-group col-12 col-sm-4 col-md-2">
+                        <div class="form-group form-group col-12 col-sm-4 col-md-3">
                             <label for="exampleFormControlSelect1">Tipo informe</label>
                             <select class="form-control form-control-sm tipo_cartera" id="tipo_informe_balance" name="tipo_informe_balance">
                                 <option value="1">De prueba</option>
@@ -21,28 +21,28 @@
                                 <option value="3">General</option>
                             </select>
                         </div>
-                        <div class="form-group col-12 col-sm-4 col-md-2">
+                        <div class="form-group col-12 col-sm-4 col-md-3">
                             <label for="exampleFormControlSelect1">Cuenta desde</label>
                             <input name="cuenta_desde_balance" id="cuenta_desde_balance" onfocus="this.select()" class="form-control form-control-sm" type="text">
                         </div>
-                        <div class="form-group col-12 col-sm-4 col-md-2">
+                        <div class="form-group col-12 col-sm-4 col-md-3">
                             <label for="exampleFormControlSelect1">Cuenta hasta</label>
                             <input name="cuenta_hasta_balance" id="cuenta_hasta_balance" onfocus="this.select()" class="form-control form-control-sm" type="text">
                         </div>
-                        <div class="form-group col-12 col-sm-4 col-md-2">
+                        <div class="form-group col-12 col-sm-4 col-md-3">
                             <label for="exampleFormControlSelect1">Cedula / Nit</label>
                             <select name="id_nit_balance" id="id_nit_balance" class="form-control form-control-sm">
                             </select>
                         </div>
-                        <div class="form-group col-12 col-sm-4 col-md-2">
+                        <div class="form-group col-12 col-sm-4 col-md-3">
                             <label for="example-text-input" class="form-control-label">Fecha desde</label>
                             <input name="fecha_desde_balance" id="fecha_desde_balance" class="form-control form-control-sm" type="date" require>
                         </div>
-                        <div class="form-group col-12 col-sm-4 col-md-2">
+                        <div class="form-group col-12 col-sm-4 col-md-3">
                             <label for="example-text-input" class="form-control-label">Fecha hasta</label>
                             <input name="fecha_hasta_balance" id="fecha_hasta_balance" class="form-control form-control-sm" type="date" require>
                         </div>
-                        <div class="form-group col-12 col-sm-4 col-md-2 row" style="margin-bottom: 0.1rem !important;">
+                        <div class="form-group col-12 col-sm-4 col-md-3 row" style="margin-bottom: 0.1rem !important;">
                             <label for="example-text-input" class="form-control-label">Niveles</label>
                             <div class="form-check col-12 col-md-12 col-sm-12" style="min-height: 0px; margin-bottom: 0px; margin-top: -2px; margin-left: 5px;">
                                 <input class="form-check-input" type="radio" name="nivel_balance" id="nivel_balance1" style="font-size: 11px;">
