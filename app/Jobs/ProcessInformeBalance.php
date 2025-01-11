baaa4f60a5a62848488d4c04a65c7a8893b216db
Abdel cartagena castro <abdel_123@hotmail.es>
2025-01-11 10:54:37 -0500

    Actualizar totales del balance general

--- app/Jobs/ProcessInformeBalance.php
+++ app/Jobs/ProcessInformeBalance.php
@@ -260,7 +260,7 @@ class ProcessInformeBalance implements ShouldQueue
                 DB::raw('SUM(saldo_anterior) AS saldo_anterior'),
                 DB::raw('SUM(debito) AS debito'),
                 DB::raw('SUM(credito) AS credito'),
-                DB::raw('SUM(saldo_final) AS saldo_final'),
+                DB::raw('SUM(saldo_anterior) + SUM(saldo_final) AS saldo_final'),
                 DB::raw('SUM(documentos_totales) AS documentos_totales')
             )
             ->orderByRaw('cuenta')
