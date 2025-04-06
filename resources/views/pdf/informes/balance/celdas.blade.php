037fa4ba51c419bb6e4768261919ad98602cafe1
Abdel cartagena castro <abdel_123@hotmail.es>
2025-04-06 11:53:28 -0500

    Agregar decimanles en pdf balance

--- resources/views/pdf/informes/balance/celdas.blade.php
+++ resources/views/pdf/informes/balance/celdas.blade.php
@@ -1,6 +1,6 @@
 <!-- A --><td style="{{ $style }}">{{ $balance->cuenta }}</td>
 <!-- A --><td style="{{ $style }}">{{ $balance->nombre_cuenta }}</td>
-<!-- B --><td style="{{ $style }} text-align: right;">{{ number_format($balance->saldo_anterior) }}</td>
-<!-- C --><td style="{{ $style }} text-align: right;">{{ number_format($balance->debito) }}</td>
-<!-- D --><td style="{{ $style }} text-align: right;">{{ number_format($balance->credito) }}</td>
-<!-- E --><td style="{{ $style }} text-align: right;">{{ number_format($balance->saldo_final) }}</td>
+<!-- B --><td style="{{ $style }} text-align: right;">{{ number_format($balance->saldo_anterior, 2) }}</td>
+<!-- C --><td style="{{ $style }} text-align: right;">{{ number_format($balance->debito, 2) }}</td>
+<!-- D --><td style="{{ $style }} text-align: right;">{{ number_format($balance->credito, 2) }}</td>
+<!-- E --><td style="{{ $style }} text-align: right;">{{ number_format($balance->saldo_final, 2) }}</td>
