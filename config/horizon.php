c0834e13f11d54324233c10f21593e5bfbd39e78
Abdel cartagena castro <abdel_123@hotmail.es>
2025-04-30 10:43:03 -0500

    Actualizar horizon

--- config/horizon.php
+++ config/horizon.php
@@ -190,8 +190,8 @@ return [
                 'maxJobs' => 0,
                 'memory' => 128,
                 'tries' => 1,
-                'timeout' => 60,
                 'nice' => 0,
+                'timeout' => 300,
             ],
             'supervisor-fast' => [
                 'connection' => 'redis',
