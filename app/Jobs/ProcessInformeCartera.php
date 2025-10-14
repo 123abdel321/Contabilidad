448e3ed13931f94036db2d5619683f372c431e8a
Abdel Cartagena <abdel_123@hotmail.es>
2025-10-14 10:41:54 -0500

    Actualizar

--- app/Jobs/ProcessInformeCartera.php
+++ app/Jobs/ProcessInformeCartera.php
@@ -835,9 +835,9 @@ class ProcessInformeCartera implements ShouldQueue
             // ->when($this->request['fecha_desde'] ? true : false, function ($query) {
 			// 	$query->where('DG.fecha_manual', '>=', $this->request['fecha_desde']);
 			// }) 
-            // ->when($this->request['fecha_hasta'] ? true : false, function ($query) {
-			// 	$query->where('DG.fecha_manual', '<=', $this->request['fecha_hasta']);
-			// })
+            ->when($this->request['fecha_hasta'] ? true : false, function ($query) {
+				$query->where('DG.fecha_manual', '<=', $this->request['fecha_hasta']);
+			})
             ->when($this->request['id_nit'] ? true : false, function ($query) {
 				$query->where('DG.id_nit', $this->request['id_nit']);
 			})
