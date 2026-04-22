9b5de4d8ec6d0faefe35674703f49b5f81027f5c
Cartagena <abdel_123@hotmail.es>
2026-04-22 13:39:45 -0500

    Actualizar edicion de usuario

--- public/assets/js/sistema/configuracion/usuarios-controller.js
+++ public/assets/js/sistema/configuracion/usuarios-controller.js
@@ -77,6 +77,8 @@ function usuariosInit() {
             var id = this.id.split('_')[1];
             var data = getDataById(id, usuarios_table);
             const permisos = data.permisos ? data.permisos : [];
+            const bodegas = permisos.length > 0 && permisos[0].ids_bodegas_responsable ? permisos[0].ids_bodegas_responsable.split(',') : [];
+            const resoluciones = permisos.length > 0 && permisos[0].ids_resolucion_responsable ? permisos[0].ids_resolucion_responsable.split(',') : [];
 
             $('#password_usuario').val('');
             $('#password_confirm').val('');
@@ -86,8 +88,8 @@ function usuariosInit() {
             $("#firstname_usuario").val(data.firstname);
             $("#lastname_usuario").val(data.lastname);
             $("#address_usuario").val(data.address);
-            $("#id_bodega_usuario").val(permisos.length > 0 ? permisos[0].ids_bodegas_responsable.split(',') : []).change();
-            $("#id_resolucion_usuario").val(permisos.length > 0 ? permisos[0].ids_resolucion_responsable.split(',') : []).change();
+            $("#id_bodega_usuario").val(bodegas).change();
+            $("#id_resolucion_usuario").val(resoluciones).change();
 
             clearPermisos();
 
