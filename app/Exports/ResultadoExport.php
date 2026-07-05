4d082f9fb90d2ffc68656c74a794271b9018c7f7
abdelcartagena <abdel_123@hotmail.es>
2026-07-05 09:19:47 -0500

    Actualizar informe de resultados

--- app/Exports/ResultadoExport.php
+++ app/Exports/ResultadoExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use DB;
+use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
@@ -25,37 +26,68 @@ class ResultadoExport implements FromView, WithColumnWidths, WithStyles, WithCol
 {
     use Exportable;
 
+    protected $cabeza;
     protected $empresa;
-
     protected $id_resultado;
+    protected $fecha_desde;
+    protected $fecha_hasta;
+    protected $mesesMostrar = [];
 
     public function __construct(int $id, $empresa)
-	{
+    {
+        $this->cabeza = InfResultado::find($id);
         $this->id_resultado = $id;
-		$this->empresa = $empresa;
-	}
+        $this->empresa = $empresa;
+        $this->fecha_desde = $this->cabeza->fecha_desde;
+        $this->fecha_hasta = $this->cabeza->fecha_hasta;
+        $this->calcularMeses();
+    }
+
+    private function calcularMeses()
+    {
+        $start = Carbon::parse($this->fecha_desde);
+        $end = Carbon::parse($this->fecha_hasta);
+        $meses = [];
+        while ($start->month <= $end->month) {
+            $meses[] = strtolower($start->locale('es')->monthName);
+            $start->addMonth();
+        }
+        $this->mesesMostrar = $meses;
+    }
 
     public function view(): View
-	{
-		return view('excel.resultado.resultado', [
-			'resultados' => InfResultadoDetalle::whereIdResultado($this->id_resultado)->get(),
-            'nivel' => InfResultado::find($this->id_resultado)->nivel,
-            'nombre_informe' => 'RESULTADOS',
-            'nombre_empresa' => $this->empresa->razon_social,
-            'logo_empresa' => $this->empresa->logo ?? 'https://app.portafolioerp.com/img/logo_contabilidad.png',
-		]);
-	}
+    {
+        $documentos = InfResultadoDetalle::whereIdResultado($this->id_resultado)->get();
+        $nit = $this->cabeza->id_nit ? Nits::find($this->cabeza->id_nit) : null;
+
+        return view('excel.resultado.resultado', [
+            'resultados' => $documentos,
+            'nivel' => $this->cabeza->nivel,
+            'mesesMostrar' => $this->mesesMostrar,
+            'encabezado' => (object)[
+                'nombre_informe' => 'Resultados',
+                'nombre_empresa' => $this->empresa->nombre_empresa,
+                'logo_empresa' => $this->empresa->logo_empresa ?? 'https://app.portafolioerp.com/img/logo_contabilidad.png',
+                'filtros' => [
+                    'Fecha' => $this->cabeza->fecha_desde || $this->cabeza->fecha_hasta
+                        ? ($this->cabeza->fecha_desde ?? 'No especificado') . ' al ' . ($this->cabeza->fecha_hasta ?? 'No especificado')
+                        : null,
+                    'Nit' => $nit ? "$nit->numero_documento - $nit->nombre_completo" : null
+                ]
+            ]            
+        ]);
+    }
 
     public function styles(Worksheet $sheet)
     {
-        $sheet->getStyle('5')->getFont()->setBold(true);
+        $ultimaLetra = $this->getUltimaColumna();
 
-        // Estilo para el nombre empresa
-        $sheet->mergeCells('B1:H1'); // Merges celdas para el título
+        // Estilo para el nombre empresa (fila 1, columna B hasta L)
+        $sheet->mergeCells("B1:{$ultimaLetra}1");
         $sheet->getStyle('B1')->applyFromArray([
             'font' => [
                 'bold' => true,
-                'size' => 30
+                'size' => 18,
             ],
             'alignment' => [
                 'horizontal' => Alignment::HORIZONTAL_LEFT,
@@ -63,12 +95,12 @@ class ResultadoExport implements FromView, WithColumnWidths, WithStyles, WithCol
             ],
         ]);
 
-        // Estilo para el título
-        $sheet->mergeCells('B2:H2'); // Merges celdas para el título
+        // Estilo para el título (fila 2, columna B hasta L)
+        $sheet->mergeCells("B2:{$ultimaLetra}2");
         $sheet->getStyle('B2')->applyFromArray([
             'font' => [
                 'bold' => true,
-                'size' => 20,
+                'size' => 16,
             ],
             'alignment' => [
                 'horizontal' => Alignment::HORIZONTAL_LEFT,
@@ -76,8 +108,8 @@ class ResultadoExport implements FromView, WithColumnWidths, WithStyles, WithCol
             ],
         ]);
 
-        // Estilo para la fecha
-        $sheet->mergeCells('B3:F3');
+        // Estilo para la fecha de generación (fila 3, columna B hasta L)
+        $sheet->mergeCells("B3:{$ultimaLetra}3");
         $sheet->getStyle('B3')->applyFromArray([
             'font' => [
                 'size' => 11,
@@ -88,8 +120,30 @@ class ResultadoExport implements FromView, WithColumnWidths, WithStyles, WithCol
             ],
         ]);
 
-        // Estilo para los encabezados (fila 4)
-        $sheet->getStyle('A5:L5')->applyFromArray([
+        // Estilo para los filtros (fila 4, columna B hasta L)
+        $sheet->mergeCells("B4:{$ultimaLetra}4");
+        $sheet->getStyle('B4')->applyFromArray([
+            'font' => [
+                'bold' => true,
+                'size' => 12,
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_LEFT,
+            ],
+        ]);
+
+        // El resto de tus estilos...
+        $sheet->getStyle("B5:{$ultimaLetra}5")->applyFromArray([
+            'font' => [
+                'size' => 11,
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_LEFT,
+            ],
+        ]);
+
+        // Estilo para los encabezados (fila 5, todas las columnas A-L)
+        $sheet->getStyle("A6:{$ultimaLetra}6")->applyFromArray([
             'font' => [
                 'bold' => true,
             ],
@@ -105,9 +159,9 @@ class ResultadoExport implements FromView, WithColumnWidths, WithStyles, WithCol
             ],
         ]);
 
-        // Aplica bordes finos a toda la tabla (desde la fila 5 en adelante)
+        // Aplica bordes finos a toda la tabla
         $highestRow = $sheet->getHighestRow();
-        $sheet->getStyle("A5:L{$highestRow}")->applyFromArray([
+        $sheet->getStyle("A6:{$ultimaLetra}{$highestRow}")->applyFromArray([
             'borders' => [
                 'allBorders' => [
                     'borderStyle' => Border::BORDER_THIN,
@@ -117,55 +171,86 @@ class ResultadoExport implements FromView, WithColumnWidths, WithStyles, WithCol
                 'vertical' => Alignment::VERTICAL_CENTER,
             ],
         ]);
+        
+        // Ajustar altura de filas del encabezado
+        $sheet->getRowDimension(1)->setRowHeight(25);
+        $sheet->getRowDimension(2)->setRowHeight(20);
+        $sheet->getRowDimension(3)->setRowHeight(15);
+        $sheet->getRowDimension(4)->setRowHeight(40);
+        
+        // Dar ancho a la columna del logo
+        $sheet->getColumnDimension('A')->setWidth(15);
     }
 
-    public function headings(): array
+    public function columnWidths(): array
     {
-        return [
-            'Cuenta',                       //A
-            'Nombre Cuenta',                //B
-            'Saldo anterior',               //C
-            'Debito',                       //D
-            'Credito',                      //E
-            'Saldo final',                  //F
-            'Ppto anterior',                //G
-            'Pptp movimiento',              //H
-            'ppto acumulado',               //I
-            'ppto diferencia',              //J
-            'ppto porcentaje',              //K
-            'Ppto porcentaje acumulado',    //L
+        $widths = [
+            'A' => 20, // Cuenta
+            'B' => 35, // Nombre Cuenta
+            'C' => 20, // Saldo anterior
         ];
+
+        $col = 'D';
+        foreach ($this->mesesMostrar as $mes) {
+            $widths[$col] = 18;
+            $col++;
+        }
+
+        // Columnas de saldo final y presupuestos
+        $widths[$col] = 20; // Saldo final
+        $col++;
+        $widths[$col] = 20; // Ppto anterior
+        $col++;
+        $widths[$col] = 20; // Ppto movimiento
+        $col++;
+        $widths[$col] = 20; // Ppto acumulado
+        $col++;
+        $widths[$col] = 20; // Ppto diferencia
+        $col++;
+        $widths[$col] = 20; // Ppto porcentaje
+        $col++;
+        $widths[$col] = 30; // Ppto porcentaje acumulado
+
+        return $widths;
     }
 
     public function columnFormats(): array
     {
-        return [
-			'C' => NumberFormat::FORMAT_CURRENCY_USD,
-			'D' => NumberFormat::FORMAT_CURRENCY_USD,
-			'E' => NumberFormat::FORMAT_CURRENCY_USD,
-			'F' => NumberFormat::FORMAT_CURRENCY_USD,
-			'G' => NumberFormat::FORMAT_CURRENCY_USD,
-			'H' => NumberFormat::FORMAT_CURRENCY_USD,
-			'I' => NumberFormat::FORMAT_CURRENCY_USD,
-			'J' => NumberFormat::FORMAT_CURRENCY_USD,
+        $formats = [
+            'C' => NumberFormat::FORMAT_CURRENCY_USD, // Saldo anterior
         ];
-	}
 
-    public function columnWidths(): array
+        $col = 'D';
+        foreach ($this->mesesMostrar as $mes) {
+            $formats[$col] = NumberFormat::FORMAT_CURRENCY_USD;
+            $col++;
+        }
+
+        // Saldo final y presupuestos (excepto porcentajes)
+        $formats[$col] = NumberFormat::FORMAT_CURRENCY_USD; // Saldo final
+        $col++;
+        $formats[$col] = NumberFormat::FORMAT_CURRENCY_USD; // Ppto anterior
+        $col++;
+        $formats[$col] = NumberFormat::FORMAT_CURRENCY_USD; // Ppto movimiento
+        $col++;
+        $formats[$col] = NumberFormat::FORMAT_CURRENCY_USD; // Ppto acumulado
+        $col++;
+        $formats[$col] = NumberFormat::FORMAT_CURRENCY_USD; // Ppto diferencia
+        // Los porcentajes no tienen formato de moneda
+
+        return $formats;
+    }
+
+    private function getUltimaColumna(): string
     {
-        return [
-            'A' => 20,
-			'B' => 35,
-			'C' => 20,
-			'D' => 20,
-			'E' => 20,
-			'F' => 20,
-			'G' => 20,
-			'H' => 20,
-			'I' => 20,
-			'J' => 20,
-			'k' => 20,
-			'l' => 30,
-        ];
-	}
+        $numColumnas = 3 + count($this->mesesMostrar) + 7; // 3 fijas iniciales + meses + 7 fijas finales
+        
+        $letra = '';
+        while ($numColumnas > 0) {
+            $mod = ($numColumnas - 1) % 26;
+            $letra = chr(65 + $mod) . $letra;
+            $numColumnas = intval(($numColumnas - $mod) / 26);
+        }
+        return $letra;
+    }
 }
--- resources/views/excel/resultado/celdas.blade.php
+++ resources/views/excel/resultado/celdas.blade.php
@@ -1,12 +1,17 @@
 <!-- A --><td style="{{ $style }}">{{ $resultado->cuenta }}</td>
 <!-- B --><td style="{{ $style }}">{{ $resultado->nombre_cuenta }}</td>
 <!-- C --><td style="{{ $style }}">{{ $resultado->saldo_anterior }}</td>
-<!-- D --><td style="{{ $style }}">{{ $resultado->debito }}</td>
-<!-- E --><td style="{{ $style }}">{{ $resultado->credito }}</td>
-<!-- F --><td style="{{ $style }}">{{ $resultado->saldo_final }}</td>
-<!-- G --><td style="{{ $style }}">{{ $resultado->ppto_anterior }}</td>
-<!-- H --><td style="{{ $style }}">{{ $resultado->ppto_movimiento }}</td>
-<!-- I --><td style="{{ $style }}">{{ $resultado->ppto_acumulado }}</td>
-<!-- J --><td style="{{ $style }}">{{ $resultado->ppto_diferencia }}</td>
-<!-- K --><td style="{{ $style }}">{{ $resultado->ppto_porcentaje }}</td>
-<!-- L --><td style="{{ $style }}">{{ $resultado->ppto_porcentaje_acumulado }}</td>
+
+<!-- Meses dinámicos -->
+@foreach($mesesMostrar as $mes)
+    <td style="{{ $style }}">{{ $resultado->{$mes} }}</td>
+@endforeach
+
+<!-- Saldo final y presupuestos -->
+<td style="{{ $style }}">{{ $resultado->saldo_final }}</td>
+<td style="{{ $style }}">{{ $resultado->ppto_anterior }}</td>
+<td style="{{ $style }}">{{ $resultado->ppto_movimiento }}</td>
+<td style="{{ $style }}">{{ $resultado->ppto_acumulado }}</td>
+<td style="{{ $style }}">{{ $resultado->ppto_diferencia }}</td>
+<td style="{{ $style }}">{{ $resultado->ppto_porcentaje }}</td>
+<td style="{{ $style }}">{{ $resultado->ppto_porcentaje_acumulado }}</td>
